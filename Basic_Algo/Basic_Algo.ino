646d0930054ba3b843692e839c269bb0e6c09b8f
KaichenWang <kaichen.wang@outlook.com>
2016-02-08 16:25:21 -0500

    add additional output variables to print in basic algorithm

--- Basic_Algo/Basic_Algo.ino
+++ Basic_Algo/Basic_Algo.ino
@@ -1,33 +1,43 @@
 float val;
 float mv;
+float temp;
 int SENSOR_PIN = 0;
 
-int const SAMPLE_INTERVAL = 1000; // in ms
-float const K = 1.0; // threshold
-float const A = 1.0; // multiplier
+int const SAMPLE_INTERVAL = 1000;
+float const K = 1.0;
+float const A = 1.0;
 
 float error = 0;
-float temp; // most recent temp on Tx side
-float tempRx; // most recent temp on Rx side
+float tempRx;
+float alpha = 0;
+
+int total = 0;
+int transmitted = 0;
 
 void setup (){
   Serial.begin(9600);
   temp = getTemp ();
   tempRx = getTemp ();
+  transmitted++;
 }
 
 void loop (){
   temp = getTemp();
+  total ++;
   error = temp - A * tempRx;
   if (abs(error) > K){
     tempRx = temp;
     Serial.print("Received: ");
+    transmitted ++;
   }
   else{
     tempRx = A * tempRx;
     Serial.print("Estimated: ");
   }
   printTemp(tempRx);
+  alpha = transmitted/total;
+  Serial.print("transmissions: ");
+  Serial.println(transmitted);
   delay(SAMPLE_INTERVAL);
 }
 
