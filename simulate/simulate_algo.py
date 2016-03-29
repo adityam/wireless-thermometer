cc7cfa235cdfcd05df17daa16751d40cd38f16df
KaichenWang <kaichen.wang@outlook.com>
2016-03-28 22:48:50 -0400

    Simulate transmansmission algorithm using vector of alphas and k_alphas

--- simulate/simulate_algo.py
+++ simulate/simulate_algo.py
@@ -1,23 +1,34 @@
+# Run simulation of transmission algorithm
+# Using collected temperature data saved in source file
+# Alpha and K_alpha are algorithm parameters
 def simulate (alpha, k_alpha):
 
+    # a is autoregressive coefficient
     a = 0.9999
 
+    # open source data file
     with open('data-batch-2.dat.txt') as data:
         
+        # create new file to export simulation results to
         results = open('sim_alpha_'+str(alpha)+'.txt', 'w+')
         results.write('alpha   = ' + str(alpha) +'\nk_alpha = ' + str(k_alpha) + '\n')
         results.close()
 
+        # write simulation results to destination file
         with open('sim_alpha_'+str(alpha)+'.txt', 'a') as results:
 
             tempTx = -1
             total = 0
             tx = 0
 
+            # for each data sample from source file
+            # simulate: 'transmit' or 'estimate'?
             for val in data:
                 temp = float(val)
                 error = temp - a * tempTx
                 total += 1
+                # transmit if error is over threshold
+                # 1st data sample is always transmitted
                 if abs(error) > k_alpha or tx == 0:
                     tx += 1
                     tempTx = temp
@@ -25,6 +36,16 @@ def simulate (alpha, k_alpha):
                 else:
                     results.write('ES: ' + str(temp) +'\n')
 
+            # write transmission statistics to end of destination file
             results.write('Tx  = ' + str(tx) + '\nTot = ' + str(total))
 
-simulate (0.10, 0.06103870595)
+
+# Run batch simulation for a vector of alpha values & corresponding k_alpha
+# Load alpha, k_alpha values from file
+with open('alpha_k_alpha.csv') as parameters:
+
+    for param in parameters:
+        pair = param.strip().split(',')
+        print 'Simulating... alpha = ' + pair[0] +', k_alpha = '+ pair[1]
+        #run simulation
+        simulate (float(pair[0]), float(pair[1]))
